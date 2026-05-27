2c3fc11601367b6c61bcb5997b84ae5cfb63e44e
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2026-05-27 18:26:41 +0900

    d

--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -87,7 +87,7 @@ const LoginPage = () => {
               </p>
           </button>
         </form>
-
+s
         <p className="sub_text">
           아직 계정이 없으신가요?{" "}
           <span className="signup_link" onClick={() => navigate("/signup")}>
